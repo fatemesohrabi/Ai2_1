2e79512696cfd3be4d347633f685c3dc786344c0
Hadi <78753020+hadiinasiri@users.noreply.github.com>
2021-02-10 23:20:59 +0330

    V1.1
    define heuristic
    define find_max_degree
    define forward_checking
    define random_pick

--- CSP.py
+++ CSP.py
@@ -1,5 +1,4 @@
-#csp
-
+import random
 class node:
     def __init__(self, value, index):
         self.value = value
@@ -8,6 +7,8 @@ class node:
         self.column = index % n
         self.deleted = False
         self.domain = [True, False]
+        self.degree = float('inf')
+##############################################################################################
 def read_file():
     infile = open("sample1.txt","r")
     global n,arr
@@ -24,4 +25,55 @@ def read_file():
             TempNode = node(int(temp[j]),index)
             arr.append(TempNode)
             index += 1
+    '''lane = 0
+    for i in range(n*n):
+        lane+=1
+        print(arr[i].index,"\t",end="")
+        if lane % 9 == 0:
+            print()'''
+##############################################################################################
+def heuristic():
+    for i in range(len(arr)):
+        if (arr[i].row == 0 and arr[i].column == 0) or (arr[i].row == 0 and arr[i].column == n-1) or (arr[i].row == n-1 and arr[i].column == 0) or (arr[i].row == n-1 and arr[i].column == n-1):
+            arr[i].degree = 2
+        elif arr[i].row == 0 or arr[i].row == n-1 or arr[i].column == 0 or arr[i].column == n-1:
+            arr[i].degree = 3
+        else:
+            arr[i].degree = 4
+##############################################################################################
+def find_max_degree():
+    maxi = float('-inf')
+    for i in range(len(arr)):
+        if arr[i].degree > maxi:
+            maxi = arr[i].degree
+    return maxi
+##############################################################################################
+def forward_checking(Node):
+    for i in range(n*n):
+        if arr[i].index == Node.index - 1 and arr[i].deleted != True:
+            arr[i].degree -= 1
+            arr[i].domain.remove(True)
+        if arr[i].index == Node.index + 1 and arr[i].deleted != True:
+            arr[i].degree -= 1
+            arr[i].domain.remove(True)
+        if arr[i].index == Node.index + n and arr[i].deleted != True:
+            arr[i].degree -= 1
+            arr[i].domain.remove(True)
+        if arr[i].index == Node.index - n and arr[i].deleted != True:
+            arr[i].degree -= 1
+            arr[i].domain.remove(True)
+##############################################################################################
+def random_pick():
+    max_deg = find_max_degree()
+    pick_arr = []
+    for i in range(n*n):
+        if arr[i].degree == max_deg:
+            pick_arr.append(arr[i])
+    random.shuffle(pick_arr)
+    return pick_arr[1]
+##############################################################################################
+#def CSP():
+    
+##############################################################################################
 read_file()
+heuristic()
